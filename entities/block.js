fe43302da02a5201f90ec91fd351f698f2e786c5
James Yen <jameshyen@gmail.com>
2017-03-02 15:46:25 -0800

    program and block

---
+++ entities/block.js
@@ -0,0 +1,11 @@
+class Block {
+
+  constructor(statements) {
+    this.statements = statements
+  }
+
+  toString() {
+    return `Block statements.join(' ')`;
+  }
+
+}
---
+++ entities/program.js
@@ -0,0 +1,11 @@
+class Program {
+
+  constructor(block) {
+    this.block = block;
+  }
+
+  toString() {
+    return `Program ${this.block}`;
+  }
+
+}
