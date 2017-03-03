42c355147a599a16bd9f5e6cc162d21182234dc3
etsai4 <etsai4@lion.lmu.edu>
2017-03-03 11:51:46 -0800

    fundec finished

---
+++ entities/fundec.js
@@ -0,0 +1,12 @@
+class FunDec{
+	
+	constructor(id, parameters, body) {
+		this.id = id;
+		this.parameters = parameters;
+		this.body = body
+	}
+
+	toString(){
+		return `FunDec ${this.id} ${this.parameters} ${body}`
+	}
+}
