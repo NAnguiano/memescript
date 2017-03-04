b3741b0de1aee8c40bc9d737f4bd73a2c8b4ad1a
NAnguiano <nanguia1@lion.lmu.edu>
2017-03-03 17:23:56 -0800

    Add a nice program to run npm start on :) Also fix an ordering issue in the relational operators and remove a console log.

--- entities/functionarguments.js
+++ entities/functionarguments.js
@@ -1,7 +1,6 @@
 class FunctionArguments {
 
   constructor(expression, args) {
-    console.log(args);
     this.expression = expression;
     if (args[0] && args[0].length > 0) {
       this.args = args[0];
