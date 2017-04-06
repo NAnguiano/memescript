e37307e8ed21edd7c6c8c68978830ccfbe732668
NAnguiano <nanguia1@lion.lmu.edu>
2017-04-06 13:11:36 -0700

    While loops.

--- entities/whileloop.js
+++ entities/whileloop.js
@@ -1,8 +1,19 @@
+const Context = require('../semantics/context');
+
 class WhileLoop {
 
   constructor(condition, body) {
     this.condition = condition;
     this.body = body;
+  }
+
+  analyze(context) {
+    const type = this.condition.analyze(context);
+    if (!type.isBoolean()) {
+      throw new Error('While loop condition must be a boolean.');
+    }
+    const innerContext = new Context({ parent: context, inLoop: true });
+    this.body.analyze(innerContext);
   }
 
   toString() {
